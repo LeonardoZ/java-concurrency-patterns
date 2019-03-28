20b9e8d4a3a35d8482308ab45dc0f6992ae10628
leonardo_zapparoli <leo.zapparoli@gmail.com>
2019-03-28 08:45:48 -0300

    Adds new Executors types of pools

--- src/main/java/br/com/leonardoz/features/executors/UsingExecutors.java
+++ src/main/java/br/com/leonardoz/features/executors/UsingExecutors.java
@@ -3,6 +3,7 @@ package br.com.leonardoz.features.executors;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,7 +16,7 @@ import java.util.concurrent.TimeUnit;
  * 
  * Executors help us to decouple task submission from execution.
  * 
- * We have 4 types of executors:
+ * We have 6 types of executors:
  * 
  * - Single Thread Executor: Uses a single worker to process tasks.
  * 
@@ -26,6 +27,13 @@ import java.util.concurrent.TimeUnit;
  * size.
  * 
  * - Scheduled Thread Pool: Bounded thread limit, used for delayed tasks.
+ * 
+ * - Single-Thread Scheduled Pool: Similar to the scheduled thread pool, but 
+ * single-threaded, with only one active task at the time. 
+ * 
+ * - Work-Stealing Thread Pool: Based on Fork/Join Framework, applies the
+ * work-stealing algorithm for balancing tasks, with available processors as a
+ * paralellism level.
  * 
  * And 2 types of tasks:
  * 
@@ -117,10 +125,10 @@ public class UsingExecutors {
 	public static void usingScheduledThreadPool() {
 		System.out.println("=== ScheduledThreadPool ===");
 		ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(4);
-		scheduledThreadPool.scheduleAtFixedRate(
-				() -> System.out.println("Print every 2s"), 0, 2, TimeUnit.SECONDS);
-		scheduledThreadPool.scheduleWithFixedDelay(
-				() -> System.out.println("Print every 2s delay"), 0, 2, TimeUnit.SECONDS);
+		scheduledThreadPool.scheduleAtFixedRate(() -> System.out.println("1) Print every 2s"), 0, 2, TimeUnit.SECONDS);
+		scheduledThreadPool.scheduleAtFixedRate(() -> System.out.println("2) Print every 2s"), 0, 2, TimeUnit.SECONDS);
+		scheduledThreadPool.scheduleWithFixedDelay(() -> System.out.println("3) Print every 2s delay"), 0, 2,
+				TimeUnit.SECONDS);
 
 		try {
 			scheduledThreadPool.awaitTermination(6, TimeUnit.SECONDS);
@@ -128,12 +136,66 @@ public class UsingExecutors {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		System.out.println("\n\n");
 	}
+	
+	public static void usingSingleTreadScheduledExecutor() {
+		System.out.println("=== SingleThreadScheduledThreadPool ===");
+		ScheduledExecutorService singleThreadScheduler = Executors.newSingleThreadScheduledExecutor();
+		singleThreadScheduler.scheduleAtFixedRate(() -> System.out.println("1) Print every 2s"), 0, 2, TimeUnit.SECONDS);
+		singleThreadScheduler.scheduleWithFixedDelay(() -> System.out.println("2) Print every 2s delay"), 0, 2,
+				TimeUnit.SECONDS);
+
+		try {
+			singleThreadScheduler.awaitTermination(6, TimeUnit.SECONDS);
+			singleThreadScheduler.shutdown();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("\n\n");
+		
+	}
+
+	public static void usingWorkStealingThreadPool() {
+		System.out.println("=== WorkStealingThreadPool ===");
+		ExecutorService workStealingPool = Executors.newWorkStealingPool();
+
+		workStealingPool.execute(() -> System.out.println("Prints normally"));
+
+		Callable<UUID> generatesUUID = UUID::randomUUID;
+		List<Callable<UUID>> severalUUIDsTasks = new LinkedList<>();
+		for (int i = 0; i < 20; i++) {
+			severalUUIDsTasks.add(generatesUUID);
+		}
+
+		try {
+			List<Future<UUID>> futureUUIDs = workStealingPool.invokeAll(severalUUIDsTasks);
+			for (Future<UUID> future : futureUUIDs) {
+				if (future.isDone()) {
+					UUID uuid = future.get();
+					System.out.println("New UUID :" + uuid);
+				}
+			}
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		try {
+			workStealingPool.awaitTermination(6, TimeUnit.SECONDS);
+			workStealingPool.shutdown();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("\n\n");
+	}
+
+	
 
 	public static void main(String[] args) {
 		usingSingleThreadExecutor();
 		usingCachedThreadPool();
 		usingFixedThreadPool();
 		usingScheduledThreadPool();
+		usingSingleTreadScheduledExecutor();
+		usingWorkStealingThreadPool();
 	}
 }
